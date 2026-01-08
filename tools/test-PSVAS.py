f4141dba5d321213ebc31af6694556c8861c7c9c
Nuo Xu <xunuo0217@gmail.com>
2026-01-08 10:17:57 +0800

    Update test-PSVAS.py

--- tools/test-PSVAS.py
+++ tools/test-PSVAS.py
@@ -24,8 +24,6 @@ from copy import deepcopy as c
 import csv
 from utils import utils
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"   
-
 parser = argparse.ArgumentParser(description='PolicyNetworkTraining')
 parser.add_argument('--lr', type=float, default=1e-4, help='learning rate') 
 parser.add_argument('--data_dir', default='dataset/', help='data directory')
